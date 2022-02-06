63c87e0ed268cc1ef488012717cfe8ca629a494f
Max Randow <maxra518@student.liu.se>
2022-02-07 00:15:25 +0100

    timer

--- src/se/liu/maxra518/lab1/Excercise1.java
+++ src/se/liu/maxra518/lab1/Excercise1.java
@@ -3,7 +3,7 @@ package se.liu.maxra518.lab1;
 public class Excercise1
 {
     public static void main(String[] args) {
-	System.out.println("Hej alla glada barn");
+        System.out.println("Hej alla glada barn");
 
     }
 }
--- src/se/liu/maxra518/tetris/TetrisViewer.java
+++ src/se/liu/maxra518/tetris/TetrisViewer.java
@@ -13,15 +13,22 @@ public class TetrisViewer
     public TetrisViewer(Board board){
 	this.board = board;
     }
-
+    private JTextArea textArea;
     public void showBoard(){
-	JTextArea textArea = new JTextArea(board.getWidth(), board.getHeight());
+	textArea = new JTextArea(board.getWidth(), board.getHeight());
 	textArea.setText(new BoardToTextConverter().convertToText(board));
 	textArea.setFont(new Font("Monospaced", Font.PLAIN, 20));
 	frame.setLayout(new BorderLayout());
 	frame.add(textArea, BorderLayout.CENTER);
 	frame.pack();
 	frame.setVisible(true);
+
+
+    }
+    public void updateBoard(Board board){
+	textArea.setText(new BoardToTextConverter().convertToText(board));
+	this.frame.revalidate();
+
 
     }
 
